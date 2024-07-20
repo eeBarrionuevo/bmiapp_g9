a0d0a3e507ae6ef24e3c26b76aceef84b40f4ebb
Elvis Barrionuevo <idcareg.g@gmail.com>
2024-07-20 10:17:23 -0500

    Agregando mi primer Widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,28 @@
 import 'package:flutter/material.dart';
 
 void main(){
-  runApp(MaterialApp());
+  runApp(MyApp());
 }
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+///Page View Screen
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
+
